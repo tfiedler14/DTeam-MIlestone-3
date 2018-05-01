db3c540a5dc8fd5c65dd7d532c8cb764256f4249
Steven Berry <berrste@gmail.com>
2018-05-01 18:25:53 -0500

    added UI from milestone 2

--- Main.java
+++ Main.java
@@ -20,42 +20,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Game rootGame;
-    
-    Main() {}
-    
+        
     public static void main(String[] args) {
-        Main main = new Main();
-        //launch(args);
-        Team[] teams = new Team[8];
-        teams[0] = new Team("team1", 1);
-        teams[0].setTeamScore(1);
-        teams[1] = new Team("team2", 2);
-        teams[1].setTeamScore(2);
-        teams[2] = new Team("team3", 3);
-        teams[2].setTeamScore(3);
-        teams[3] = new Team("team4", 4);
-        teams[3].setTeamScore(4);
-        teams[4] = new Team("team5", 5);
-        teams[4].setTeamScore(5);
-        teams[5] = new Team("team6", 6);
-        teams[5].setTeamScore(6);
-        teams[6] = new Team("team7", 7);
-        teams[6].setTeamScore(7);
-        teams[7] = new Team("team8", 8);
-        teams[7].setTeamScore(8);
-        main.fillTournament(teams);
-        //main.fillTournament(parseTeams(args[0]));
+        launch(args);
+        
     }
     
     public void fillTournament(Team[] teams) {
         rootGame = new Game();
         rootGame = fillPrevGames(0, 0, teams);
         rootGame = updateTree(rootGame);
-        rootGame.getPrevGame1().getTeam1().setTeamScore(9);
-        rootGame.getPrevGame1().getTeam2().setTeamScore(10);
-        rootGame = updateTree(rootGame);
-        rootGame.getTeam1().setTeamScore(11);
-        System.out.println(rootGame.getTeam1().getTeamName());
     }
     
     private Game fillPrevGames(int height, int width, Team[]teams) {
@@ -78,7 +52,114 @@ public class Main extends Application {
     }
     
     public void start(Stage primaryStage) {
+        Team[] teams = new Team[8]; //for testing
+        teams[0] = new Team("team1", 1);
+        teams[0].setTeamScore(1);
+        teams[1] = new Team("team2", 2);
+        teams[1].setTeamScore(2);
+        teams[2] = new Team("team3", 3);
+        teams[2].setTeamScore(3);
+        teams[3] = new Team("team4", 4);
+        teams[3].setTeamScore(4);
+        teams[4] = new Team("team5", 5);
+        teams[4].setTeamScore(5);
+        teams[5] = new Team("team6", 6);
+        teams[5].setTeamScore(6);
+        teams[6] = new Team("team7", 7);
+        teams[6].setTeamScore(7);
+        teams[7] = new Team("team8", 8);
+        teams[7].setTeamScore(8);
+        fillTournament(teams);
         
+        int initialTeams = teams.length;
+        primaryStage.setTitle("DTeam");
+        Label title = new Label();
+        title.setText("Tournament");
+        title.setFont(new Font(20.0));
+        GridPane grid = new GridPane();
+        grid.setVgap(0);
+        grid.setHgap(15);
+        int rounds = (int) (Math.log(initialTeams) / Math.log(2));
+        
+        for (int round = 0; round < rounds; round++) {
+            
+            int numberOfGames = (int) (initialTeams/(Math.pow(2, round+1)));
+            int spaces = ((initialTeams/2)-numberOfGames)/2;
+            
+            for (int i = 0; i < numberOfGames; i++) {
+                //Game i
+                VBox game1 = new VBox();
+                game1.setSpacing(2);
+                //Team 1
+                HBox team1 = new HBox();
+                team1.setAlignment(Pos.CENTER_LEFT);
+                team1.setSpacing(5);
+                //Game Label
+                Label gameLabel = new Label();
+                gameLabel.setText("Game " + (round+1) + "-" + (i+1));
+                gameLabel.setMinWidth(75);
+                gameLabel.setFont(new Font(18));
+                //Team 1 Label
+                Label team1Label = new Label();
+                team1Label.setText(teams[2*i].getTeamName()); //remove once we are able to fill it with actual data
+                team1Label.setMinWidth(75.0);
+                //Team 1 Field
+                TextField team1Field = new TextField();
+                team1Field.setPromptText("Score");
+                
+                //Team 2
+                HBox team2 = new HBox();
+                team2.setAlignment(Pos.CENTER_LEFT);
+                team2.setSpacing(5);
+                //Team 2 Label
+                Label team2Label = new Label();
+                team2Label.setText(teams[2*i + 1].getTeamName()); //remove once we are able to fill it with actual data
+                team2Label.setMinWidth(75.0);
+                //Team 2 Field
+                TextField team2Field = new TextField();
+                team2Field.setPromptText("Score");
+                //Buttons HBox
+                HBox buttons = new HBox();
+                buttons.setAlignment(Pos.CENTER_LEFT);
+                buttons.setSpacing(5);
+                //Reset Button
+                Button reset = new Button();
+                reset.setText("Reset Game");
+               reset.setOnAction(new EventHandler<ActionEvent>(){
+                    @Override public void handle(ActionEvent e){
+                        team1Field.setText("");
+                        team2Field.setText("");
+                    }
+                });
+             
+                //Submit Button
+                Button submit = new Button();
+                submit.setText("Submit");
+                
+                buttons.getChildren().addAll(reset, submit);
+                
+                team1.getChildren().addAll(team1Label, team1Field);
+                
+                team2.getChildren().addAll(team2Label, team2Field);
+                
+                game1.getChildren().addAll(gameLabel, team1, team2, buttons);
+                
+                grid.add(game1, round, i+spaces);
+            }
+        }
+        
+        ScrollPane sp = new ScrollPane();      
+        sp.setContent(grid);
+        sp.setPrefWidth(1370);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+     
+        BorderPane root = new BorderPane();
+        root.setTop(title);
+        root.setCenter(grid);
+        root.setRight(sp);
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
     }
     
     public Game updateTree(Game curr) {
@@ -100,24 +181,7 @@ public class Main extends Application {
     }
     
     private static Team[] parseTeams(String arg) {
-        Team[] teams = new Team[4];
-        teams[0] = new Team("team1", 1);
-        teams[0].setTeamScore(1);
-        teams[1] = new Team("team2", 2);
-        teams[1].setTeamScore(2);
-        teams[2] = new Team("team3", 3);
-        teams[2].setTeamScore(3);
-        teams[3] = new Team("team4", 4);
-        teams[3].setTeamScore(4);
-        teams[0] = new Team("team5", 5);
-        teams[0].setTeamScore(5);
-        teams[1] = new Team("team6", 6);
-        teams[1].setTeamScore(6);
-        teams[2] = new Team("team7", 7);
-        teams[2].setTeamScore(7);
-        teams[3] = new Team("team8", 8);
-        teams[3].setTeamScore(8);
-        return teams;
+        return null; // TODO
     }
 
 }
